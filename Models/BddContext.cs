84dc16699fa4197ac789a9199c9324378e7baeb2
@DavDav025 <davaitcheou@gmail.com>
2023-05-25 17:33:53 +0200

    Upload project

---
+++ Models/BddContext.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeVoyage.Models
+{
+	
+        public class BddContext : DbContext
+        {
+            public DbSet<Evenement> Evenements { get; set; }
+            //public DbSet<Catalogue> Catalogues{ get; set; }
+            public DbSet<Itineraire> Itineraires { get; set; }
+            public DbSet<Membre> Membres { get; set; }
+            public DbSet<OffreVoyage> OffreVoyages { get; set; }
+            public DbSet<Partenaire> Partenaires { get; set; }
+            public DbSet<Reservation> Reservations { get; set; }
+            public DbSet<Service> Services { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+            optionsBuilder.UseMySql("server=localhost;user id=root;password=Kuroikage@25;database=CodeVoyageBDD");
+            }
+
+        public void InitializeDb()
+        {
+            this.Database.EnsureDeleted();
+            this.Database.EnsureCreated();
+            this.Evenements.AddRange(
+                new Evenement
+                {
+                    Id = 1,
+                    Nom = "Carnaval de Rio",
+                    Date = new DateTime(2024, 02, 09),
+                    Localisation = "Rio",
+                    TypeEvenement = ((TypeEvenement)3)
+                },
+                new Evenement
+                {
+                    Id = 2,
+                    Nom = "Caribana festival",
+                    Date = new DateTime(2023, 07, 01),
+                    Localisation = "Toronto",
+                    TypeEvenement = ((TypeEvenement)3)
+
+                },
+                new Evenement
+                {
+                    Id = 3,
+                    Nom = "Atlanta Arts",
+                    Date = new DateTime(2023, 09, 01),
+                    Localisation = "Piedmont Park",
+                    TypeEvenement = ((TypeEvenement)1)
+
+                },
+                new Evenement
+                {
+                    Id = 4,
+                    Nom = "San Francisco Fall Antiques Show",
+                    Date = new DateTime(2023, 10, 01),
+                    Localisation = "San Francisco",
+                    TypeEvenement = ((TypeEvenement)1)
+
+                }
+
+                );
+                this.Itineraires.AddRange(
+                new Itineraire
+                {
+                    Id = 1,
+                    DateDepart = new DateTime(2024, 02, 07),
+                    DateArrivee = new DateTime(2024, 02, 21),
+                    LieuDepart = "Paris",
+                    Destination = "Rio De Janeiro",
+                    NombreVoyageur = 40,
+                    Transport = ((MoyenDeTransport)0),
+                    Prix = 560
+                },
+                new Itineraire
+                {
+                    Id = 2,
+                    DateDepart = new DateTime(2023, 06, 30),
+                    DateArrivee = new DateTime(2023, 07, 14),
+                    LieuDepart = "Paris",
+                    Destination = "Toronto",
+                    NombreVoyageur = 50,
+                    Transport = ((MoyenDeTransport)0),
+                    Prix = 575
+                },
+                new Itineraire
+                {
+                    Id = 3,
+                    DateDepart = new DateTime(2023, 10, 15),
+                    DateArrivee = new DateTime(2023, 11, 01),
+                    LieuDepart = "Paris",
+                    Destination = "San Francisco",
+                    NombreVoyageur = 30,
+                    Transport = ((MoyenDeTransport)0),
+                    Prix = 560
+                });
+
+            this.Services.AddRange(
+                new Service
+                {
+                    Id = 1,
+                    nomService= "Visite touristique",
+                    Capacite= 20,
+                    Date= new DateTime(2023, 10, 21),
+                    TypeService=((TypeService)4),
+                    Prix = 10
+                },
+                new Service
+                {
+                    Id = 2,
+                    nomService = "Mini golf",
+                    Capacite = 9,
+                    Date = new DateTime(2023, 07, 21),
+                    TypeService = ((TypeService)2),
+                    Prix = 12
+                },
+                new Service
+                {
+                    Id = 3,
+                    nomService = "Airbnb",
+                    Capacite = 2,
+                    Date = new DateTime(2023, 08, 21),
+                    TypeService = ((TypeService)1),
+                    Prix = 140
+                });;
+
+
+            this.SaveChanges();
+        }
+
+    }
+    
+}
+
